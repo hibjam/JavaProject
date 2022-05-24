38b58b496f9b756c840266fa13afd6fc2ff1b88f
J Hibbert <jhibbert@spartaglobal.com>
2022-05-24 15:19:40 +0100

    Palindrome and Bubble Sort Commits

--- src/main/java/com/sparta/jh/BubbleSort/BubbleSort.java
+++ src/main/java/com/sparta/jh/BubbleSort/BubbleSort.java
@@ -4,22 +4,22 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int arr[] ={29,34365,63,12,23,45,320,5};
-        bubbleSort(arr);//sorting array elements using bubble sort
+        int array[] ={29,34365,63,12,23,45,320,5};
+        bubbleSort(array);//sorting array elements using bubble sort
         System.out.println("Array After Bubble Sort");
-        for(int i=0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
+        for(int i=0; i < array.length; i++){
+            System.out.print(array[i] + " ");
         }
         //iterates over every item in the array and prints on same line with space in between
     }
-    public static void bubbleSort(int[] arr) {
+    public static void bubbleSort(int[] array) {
         int swap = 0;
-        for(int i=0; i < arr.length; i++){
-            for(int j=1; j < (arr.length-i); j++){
-                if(arr[j-1] > arr[j]){
-                    swap = arr[j-1]; // swap variable becomes no. located a j-1
-                    arr[j-1] = arr[j]; // j-1 becomes number located ahead of it
-                    arr[j] = swap; // j becomes number that was located behind it (prev assigned to swap var)
+        for(int i=0; i < array.length; i++){
+            for(int x=1; x < (array.length-i); x++){
+                if(array[x-1] > array[x]){
+                    swap = array[x-1]; // swap variable becomes no. located a x// -1
+                    array[x-1] = array[x]; // x-1 becomes number located ahead of it
+                    array[x] = swap; // x becomes number that was located behind it (prev assigned to swap var)
                 }
             }
         }
---
+++ src/test/java/com/sparta/jh/PalindromeTest.java
@@ -0,0 +1,19 @@
+package com.sparta.jh;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PalindromeTest {
+    String wordToCheck = "Hannah";
+
+    @Test
+    void isPalindrome(String wordToCheck) {
+        Assertions.ass;
+    }
+
+    @Test
+    void reverse() {
+    }
+}
